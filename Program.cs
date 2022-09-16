f0021f37023d1594540215c51317c7eab0082316
Maria <masya.2506@mail.ru>
2022-09-16 20:07:11 +0300

    Add_Comments

--- Program.cs
+++ Program.cs
@@ -2,30 +2,29 @@
 
 Console.Write("Введите массив строк через пробел:");
 string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                            .ToArray();
+                                     .ToArray();
+PrintArray(array); // вывод входного массива
+Console.WriteLine(); 
+int col = sizeNewArray(array); // размерность нового массива
+PrintArray(GetArray(array, col)); // вывод выходного массива
 
-PrintArray(array);
-Console.WriteLine();
-int col = sizeNewArray(array);
-PrintArray(GetArray(array, col));
-
-int sizeNewArray(string[] inNewArray)
+int sizeNewArray(string[] inNewArray) // подсчет количества элементов входного массива, длина которых меньше или равна 3
 {
     int count = 0;
     for (int i = 0; i < inNewArray.Length; i++)
     {
         if (inNewArray[i].Length <= 3) 
         {
-            count++;
+            count++; // счетчик размерности выходного массива
         }    
     }
     return count;
 }
 
-string[] GetArray(string[] inNewArr, int size)
+string[] GetArray(string[] inNewArr, int size) // формирование выходного массива
 {
     string[] NewArray = new string[size];
-    size = 0;
+    size = 0; // обнуляем размерность для поочердного заполнения выходного массива
     for (int j = 0; j < inNewArr.Length; j++)
     {
         if (inNewArr[j].Length <= 3) 
@@ -37,7 +36,7 @@ string[] GetArray(string[] inNewArr, int size)
     return NewArray;
 }
 
-void PrintArray(string[] inArray) // печать матрицы
+void PrintArray(string[] inArray) // печать массива
 {
     Console.Write("[");
     for (int i = 0; i < inArray.Length; i++)
